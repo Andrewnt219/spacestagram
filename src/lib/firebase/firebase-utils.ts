13b1012d5e87da4d0f811a3495e67a108ccc92f6
Andrew <phongnguyentuan20@gmail.com>
2021-09-18 17:39:58 -0400

    fixed init firestore failed

--- src/lib/firebase/firebase-utils.ts
+++ src/lib/firebase/firebase-utils.ts
@@ -1,6 +1,6 @@
-export const increment = FirebaseFirestore.FieldValue.increment(
-  1
-) as unknown as number;
-export const decrement = FirebaseFirestore.FieldValue.increment(
-  1
+import { firestore } from 'firebase-admin';
+
+export const increment = firestore.FieldValue.increment(1) as unknown as number;
+export const decrement = firestore.FieldValue.increment(
+  -1
 ) as unknown as number;
